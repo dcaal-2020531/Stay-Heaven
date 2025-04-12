8a82abd74140fb7b7eafd4537f050ecee739cf3b
joroxon-2020299 <joroxon-2020299@kinal.edu.gt>
2025-04-11 19:41:53 -0600

    Se sube modelo de admin

---
+++ src/admin/admin.model.js
@@ -0,0 +1,44 @@
+import { Schema, model } from 'mongoose'
+
+const adminSchema = Schema(
+    { 
+        name: {
+            type: String,
+            required: [true, 'Name is required'],
+            maxLength: [25, `Can't overcome 25 characters`],
+        },
+        surname: {
+            type: String,
+            required: [true, 'Surname is required'],
+            maxLength: [25, `Can't overcome 25 characters`],
+        },
+        username: {
+            type: String,
+            required: [true, 'Username is required'],
+            unique: true, 
+            maxLength: [20, `Can't overcome 20 characters`],
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: [true, 'Email is required'],
+            lowercase: true
+        },
+        password: {
+            type: String,
+            required: [true, 'Password is required'],
+            minLength: [8, 'Password must be at least 8 characters']
+        },
+        role: { 
+            type: String, 
+            default: 'ADMIN' 
+        },
+        phone: {
+            type: number,
+            required: [true, 'Phone is required']
+        }
+    }
+)
+
+
+export default model('Admin', adminSchema)
