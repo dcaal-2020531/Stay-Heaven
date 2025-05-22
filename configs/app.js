0e215be1bbb2a425f64ad279c3a1ee650a55ca13
aborja-2020413 <aborja-2020413@Kinal.edu.gt>
2025-05-21 22:35:11 -0600

    Se configura archivo app

--- configs/app.js
+++ configs/app.js
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-'use strict'
-
-import express from 'express' //Servidor HTTP
-import morgan from 'morgan' //Logs
-import helmet from 'helmet' //Seguridad para HTTP
-import cors from 'cors'
-import hotelRoutes from '../src/hotel/hotel.routes.js'
-import clientRoutes from '../src/Client/client.routes.js'
-
-const configs = (app)=>{
-    app.use(express.json()) //Aceptar y enviar datos en JSON
-    app.use(express.urlencoded({extended: false})) //No encriptar la URL
-    app.use(cors())
-    app.use(helmet())
-    app.use(morgan('dev'))
-}
-
-    const routes = (app)=>{
-        app.use('/', hotelRoutes)
-        app.use('/client', clientRoutes)
-    }
-
-export const initServer = async()=>{
-    const app = express()
-    try{
-        configs(app)
-=======
 'use estrict'
 
 import express from 'express' //SERVIDOR HTTP
@@ -53,7 +25,6 @@ export const initServer = async()=>{
     const app = express() //INSTANCIA DE EXPRESS
     try{
         configs(app) //APLICAR CONFIGURACIONES DEL SERVIDOR 
->>>>>>> a-borja
         routes(app)
         app.listen(process.env.PORT)
         console.log(`Server running in port ${process.env.PORT}`)
@@ -61,3 +32,4 @@ export const initServer = async()=>{
         console.error('Server init failed', err)
     }
 }
+
